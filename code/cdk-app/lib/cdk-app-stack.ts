fe3061ff114d6f1a0f7c3678e1d9901e7646dc63
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-02-10 11:04:17 +0900

    create change value method

--- code/cdk-app/lib/cdk-app-stack.ts
+++ code/cdk-app/lib/cdk-app-stack.ts
@@ -1,8 +1,34 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as path from "path";
+import * as fs from "fs";
 
 export class CdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // 擬似パラメータインスタンス作成
+    const pseudo = new cdk.ScopedAws(this);
+
+    // JSONファイルパス
+    const filePath = path.join(`${__dirname}`, "./json/test-policy.json");
+
+    // JSON文字列をJavaScriptオブジェクトに変換
+    const jsonData = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
+    // 文字列置換
+    const jsonPolicy = JSON.parse(
+      JSON.stringify(jsonData)
+        .replace(/{Partition}/g, pseudo.partition)
+        .replace(/{Region}/g, pseudo.region)
+        .replace(/{Account}/g, pseudo.accountId)
+    );
+
+    // IAMポリシー作成
+    new iam.ManagedPolicy(this, "policy", {
+      managedPolicyName: "iam-policy",
+      document: iam.PolicyDocument.fromJson(jsonPolicy),
+    });
   }
 }
